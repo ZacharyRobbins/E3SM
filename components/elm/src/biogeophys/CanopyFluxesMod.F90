5b9138f922846b8858cd2485093d80a7c5354922
ZacharyRobbins <zjrobbin@NCSU.edu>
2024-02-20 13:48:20 -0700

    Update CanopyFluxesMod.F90
    Adding in wrapper to calculate tmax y tmin

--- components/elm/src/biogeophys/CanopyFluxesMod.F90
+++ components/elm/src/biogeophys/CanopyFluxesMod.F90
@@ -878,6 +878,10 @@ contains
                   svpts(begp:endp), eah(begp:endp), o2(begp:endp), &
                   co2(begp:endp), rb(begp:endp), dayl_factor(begp:endp), &
                   atm2lnd_vars, temperature_vars, canopystate_vars, photosyns_vars)
+            call alm_fates%wrap_insects(bounds, fn, filterp(1:fn), &
+                  svpts(begp:endp), eah(begp:endp), o2(begp:endp), &
+                  co2(begp:endp), rb(begp:endp), dayl_factor(begp:endp), &
+                  atm2lnd_vars, temperature_vars, canopystate_vars, photosyns_vars)	
 #endif
          else ! not use_fates
 
